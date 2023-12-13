86307ba7b4702635498d9c4524c38c67eb23ed37
alemasi1 <150929422+alemasi1@users.noreply.github.com>
2023-12-13 15:54:17 +0100

    upload entire repository
    questi sono tutti i file con cui stavo lavorando quindi se dovete cambiare qualcosa cambiatela clonando l'intera rep e lavorandoci dentro 
    ## Cose da fare
    - ho finito padova cambiate il div "description" a modo (semplice html)
    - per quanto riguarda lo slideshow c'è un array sotto all'interno del tag <script> con il nome dei file di tutte le immagini should be pretty intuitive. 
    - per risolvere il problema del (penso) delay della funzione ho settato alla base della classe un'immagine background sopra nello style c'è
    ```
    .jumbotron {
            background-size: cover;
            background-position: center;
            background-image: url("static/images/prato.jpg");  //cambiare questa qua
            color: #fff;
            text-align: center;
            padding: 100px 20px;
            margin-bottom: 0;
            position: relative;
            overflow: hidden;
            height: 700px;
        }
    ```

---
+++ mcm_groupwork/frontend/app/main.py
@@ -0,0 +1,95 @@
+"""
+Frontend module for the Flask application.
+
+This module defines a simple Flask application that serves as the frontend for the project.
+"""
+
+from flask import Flask, render_template
+import requests  # Import the requests library to make HTTP requests
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
+
+# Configuration for the FastAPI backend URL
+FASTAPI_BACKEND_HOST = 'http://backend'  # Replace with the actual URL of your FastAPI backend
+BACKEND_URL = f'{FASTAPI_BACKEND_HOST}/query/'
+
+
+class QueryForm(FlaskForm):
+    person_name = StringField('Person Name:')
+    submit = SubmitField('Get Birthday from FastAPI Backend')
+
+
+@app.route('/')
+def index():
+    """
+    Render the index page.
+
+    Returns:
+        str: Rendered HTML content for the index page.
+    """
+    # Fetch the date from the backend
+    
+    return render_template('index.html')
+
+@app.route('/padova')
+def padova():
+    return render_template('padova.html')
+
+@app.route('/venezia')
+def venezia():
+    return render_template('venezia.html')
+
+@app.route('/verona')
+def verona():
+    return render_template('verona.html')
+
+@app.route('/belluno')
+def belluno():
+    return render_template('belluno.html')
+
+@app.route('/treviso')
+def treviso():
+    return render_template('treviso.html')
+
+@app.route('/vicenza')
+def vicenza():
+    return render_template('vicenza.html')
+
+@app.route('/rovigo')
+def rovigo():
+    return render_template('rovigo.html')
+
+@app.route('/internal', methods=['GET', 'POST'])
+def internal():
+    """
+    Render the internal page.
+
+    Returns:
+        str: Rendered HTML content for the index page.
+    """
+    form = QueryForm()
+    error_message = None  # Initialize error message
+
+    if form.validate_on_submit():
+        person_name = form.person_name.data
+
+        # Make a GET request to the FastAPI backend
+        fastapi_url = f'{FASTAPI_BACKEND_HOST}/query/{person_name}'
+        response = requests.get(fastapi_url)
+
+        if response.status_code == 200:
+            # Extract and display the result from the FastAPI backend
+            data = response.json()
+            result = data.get('birthday', f'Error: Birthday not available for {person_name}')
+            return render_template('internal.html', form=form, result=result, error_message=error_message)
+        else:
+            error_message = f'Error: Unable to fetch birthday for {person_name} from FastAPI Backend'
+
+    return render_template('internal.html', form=form, result=None, error_message=error_message)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
