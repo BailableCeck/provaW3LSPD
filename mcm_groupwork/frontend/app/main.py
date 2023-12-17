744153622969da9b4a2f77f0d3a07f67335ab1ce
Martina <888698@stud.unive.it>
2023-12-17 14:03:49 +0100

    Updated code using pycodestyle
    Errors remaining:
    main.py:4:52: W291 trailing whitespace
    main.py:23:1: E302 expected 2 blank lines, found 1
    main.py:96:80: E501 line too long (261 > 79 characters)
    main.py:110:80: E501 line too long (103 > 79 characters)
    main.py:118:80: E501 line too long (94 > 79 characters)
    main.py:120:80: E501 line too long (90 > 79 characters)
    main.py:124:80: E501 line too long (120 > 79 characters)
    main.py:126:80: E501 line too long (153 > 79 characters)
    main.py:128:80: E501 line too long (102 > 79 characters)
    main.py:130:80: E501 line too long (125 > 79 characters)
    main.py:132:1: E305 expected 2 blank lines after class or function definition, found 1
    main.py:134:1: W391 blank line at end of file

--- mcm_groupwork/frontend/app/main.py
+++ mcm_groupwork/frontend/app/main.py
@@ -1,7 +1,8 @@
 """
 Frontend module for the Flask application.
 
-This module defines a simple Flask application that serves as the frontend for the project.
+This module defines a simple Flask application that 
+serves as the frontend for the project.
 """
 
 from flask import Flask, render_template
@@ -12,15 +13,13 @@ from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
+app.config['SECRET_KEY'] = 'your_secret_key'
 
-"""Configuration for the FastAPI backend URL"""
-FASTAPI_BACKEND_HOST = 'http://backend'  
+# Configuration for the FastAPI backend URL
+FASTAPI_BACKEND_HOST = 'http://backend'
 BACKEND_URL = f'{FASTAPI_BACKEND_HOST}/query/'
 
-
-
-""" Form class to handle query input from the user"""
+# Form class to handle query input from the user
 class QueryForm(FlaskForm):
 
     person_name = StringField('Destination:')
@@ -41,49 +40,44 @@ def index():
     Returns:
         str: Rendered HTML content for the index page.
     """
-    
-    #Fetch the date from the backend
-    
+    # Fetch the data from the backend
     return render_template('index.html')
+
 
 @app.route('/padova')
 def padova():
     return render_template('padova.html')
 
+
 @app.route('/venezia')
 def venezia():
     return render_template('venezia.html')
+
 
 @app.route('/verona')
 def verona():
     return render_template('verona.html')
 
+
 @app.route('/belluno')
 def belluno():
     return render_template('belluno.html')
+
 
 @app.route('/treviso')
 def treviso():
     return render_template('treviso.html')
 
+
 @app.route('/vicenza')
 def vicenza():
     return render_template('vicenza.html')
+
 
 @app.route('/rovigo')
 def rovigo():
     return render_template('rovigo.html')
 
-"""
-    Handle the internal page form submission and render the results.
-
-    This function handles form submission from the internal page,
-    interacts with the FastAPI backend based on form inputs
-    and renders the results on the internal page.
-
-    Returns:
-        str: Rendered HTML content for the internal page.
-    """
 
 @app.route('/internal', methods=['GET', 'POST'])
 def internal():
@@ -92,29 +86,25 @@ def internal():
 
     if form.validate_on_submit():
         comune = form.person_name.data
-
-        """Obtain checkbox's values from form"""
         piscina_filter = form.piscina_checkbox.data
-
         accesso_disabili_filter = form.accesso_disabili_checkbox.data
         fitness_filter = form.fitness_checkbox.data
         sauna_filter = form.sauna_checkbox.data
         aria_condizionata_filter = form.aria_condizionata_checkbox.data
         animali_amessi_filter = form.animali_amessi_checkbox.data
 
-        # Aggiorna l'URL per includere i filtri
-        fastapi_url = f'{FASTAPI_BACKEND_HOST}/query/{comune}?piscina={piscina_filter}&accesso_disabili={accesso_disabili_filter}&fitness={fitness_filter}&sauna={sauna_filter}&aria_condizionata={aria_condizionata_filter}&animali_ammessi={animali_amessi_filter}'  
+        # Update URL to include filters
+        fastapi_url = f'{FASTAPI_BACKEND_HOST}/query/{comune}?piscina={piscina_filter}&accesso_disabili={accesso_disabili_filter}&fitness={fitness_filter}&sauna={sauna_filter}&aria_condizionata={aria_condizionata_filter}&animali_ammessi={animali_amessi_filter}'
 
-        """Make a GET request to the FastAPI backend"""
+        # Make a GET request to the FastAPI backend
         response = requests.get(fastapi_url)
 
         if response.status_code == 200:
-            """Extract and display the result from the FastAPI backend"""
+            # Extract and display the result from the FastAPI backend
             data = response.json()
             accomodations = data.get('risultati', [])
-           
+
             if accomodations:
-                
                 result_strutture = []
                 for struttura in accomodations:
                     if "link" in struttura:
@@ -122,24 +112,24 @@ def internal():
                     else:
                         result_strutture.append({"nome": struttura["nome"]})
 
-                """Debug print to check the value of result_strutture"""
+                # Debug print to check the value of result_strutture
                 print("Result_strutture:", result_strutture)
 
                 if not result_strutture:
-                    result_strutture = [{"nome": f'No accomodations available for {comune}'}]
+                    result_strutture = [{"nome": f'No accommodations available for {comune}'}]
             else:
-                result_strutture = [{"nome": f'No accomodations available for {comune}'}]
+                result_strutture = [{"nome": f'No accommodations available for {comune}'}]
 
-            """ Extract museum's information """
+            # Extract museum's information
             musei_consigliati = data.get('musei_consigliati', [])
             result_musei = ', '.join(musei_consigliati) if musei_consigliati else f'No recommended museums for {comune}'
 
             return render_template('internal.html', form=form, result_strutture=result_strutture, result_musei=result_musei, error_message=error_message)
         else:
-            error_message = f'Error: Unable to fetch accommodation for {comune} from FastAPI Backend'
+            error_message = f'Error: Unable to fetch accommodations for {comune} from FastAPI Backend'
 
     return render_template('internal.html', form=form, result_strutture=None, result_musei=None, error_message=error_message)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
-    
+
