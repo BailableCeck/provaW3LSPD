27c30359749b2049f5e3552f2d2880dfd59e6d0a
alemasi1 <150929422+alemasi1@users.noreply.github.com>
2023-12-17 19:31:13 +0100

    Add files via upload

---
+++ provaW3LSPD/mcm_groupwork/backend/tests/test_main.py
@@ -0,0 +1,68 @@
+import os
+import sys
+from fastapi.testclient import TestClient
+from fastapi import status
+import pandas as pd
+
+
+# Add the project root to the sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Now you can do the relative import
+from app.main import app
+
+"""
+Execute this test by running on the terminal (from the app/) the command:
+pytest --cov=app --cov-report=html tests/
+ """
+
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Funziona?": "World"}
+
+
+""" def test_fail_read_item():
+    response = client.get("/query/Pippo")
+    assert response.status_code == 200
+    assert response.json() == {"error": "Person not found"} """
+
+
+# The following will generate an error in pycheck
+""" def test_success_read_item_module():
+    response = client.get("/module/search/Albert Einstein")
+    assert response.status_code == 200
+    assert response.json() == {"Albert Einstein's birthday is 03/14/1879."} """
+
+
+# Defining 4 tests
+def test_query_endpoint_success():
+    comune = "Monfumo"
+    response = client.get(f"/query/{comune}")
+    assert response.json() == {"comune": 'MONFUMO', 'musei_consigliati':[], 
+                               "risultati": [{'nome': 'DA GERRY LOCANDA', 'link': 'www.ristorantedagerry.com'}, {'nome': "AGRITURISMO GHISOLANA - DALL'EST LISA", 'link': 'www.agriturismoalcapitello.it'}, {'nome': 'CASA ROSA AGRITURISMO - GIRARDI GIUSEPPINA'}]}
+
+def test_query_endpoint_no_results():
+    comune = "ComuneInesistente"
+    response = client.get(f"/query/{comune}")
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert data["error"] == "Alloggio non trovato"
+    assert "risultati" not in data
+    assert "musei_consigliati" not in data
+
+def test_query_endpoint_with_piscina():
+    comune = "zero branco"
+    response = client.get(f"/query/{comune}?piscina=true")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"comune": 'ZERO BRANCO', 'musei_consigliati':[],
+                               "risultati": [{'nome': 'GIUSTO ELISA - B&B SOLOQUI', 'link': 'www.soloqui.com'}]}
+
+def test_query_endpoint_musei_only():
+    comune = "nove"
+    response = client.get(f"/query/{comune}?piscina=true")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"comune": 'NOVE', 'musei_consigliati':['Museo della Ceramica'],"risultati":[]}
---
+++ provaW3LSPD/mcm_groupwork/frontend/app/main.py
@@ -0,0 +1,191 @@
+"""
+Frontend module for the Flask application.
+
+This module defines a simple Flask application that 
+serves as the frontend for the project.
+"""
+
+from flask import Flask, render_template
+import requests  # Import the requests library to make HTTP requests
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired
+from flask import request
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+"""Configuration for the FastAPI backend URL."""
+FASTAPI_BACKEND_HOST = 'http://backend'
+BACKEND_URL = f'{FASTAPI_BACKEND_HOST}/query/'
+
+class QueryForm(FlaskForm):
+    """
+    Form class to handle query input from the user.
+    """
+
+    person_name = StringField('Destination:')
+    piscina_checkbox = BooleanField('Piscina')
+    accesso_disabili_checkbox = BooleanField('Accesso Disabili')
+    fitness_checkbox = BooleanField('Fitness')
+    sauna_checkbox = BooleanField('Sauna Checkbox')
+    aria_condizionata_checkbox = BooleanField('Aria Condizionata')
+    animali_amessi_checkbox = BooleanField('Animali Ammessi')
+    submit_field = SubmitField('Search')
+
+
+@app.route('/')
+def index():
+    """
+    Render the index page.
+
+    Returns:
+        str: Rendered HTML content for the index page.
+    """
+    # Fetch the data from the backend
+    return render_template('index.html')
+
+
+@app.route('/padova')
+def padova():
+    """
+    Render the Padova page.
+
+    Returns:
+        str: Rendered HTML content for the Padova page.
+    """
+    return render_template('padova.html')
+
+
+@app.route('/venezia')
+def venezia():
+    """
+    Render the Venezia page.
+
+    Returns:
+        str: Rendered HTML content for the Venezia page.
+    """
+    return render_template('venezia.html')
+
+
+@app.route('/verona')
+def verona():
+    """
+    Render the Verona page.
+
+    Returns:
+        str: Rendered HTML content for the Verona page.
+    """
+    return render_template('verona.html')
+
+
+@app.route('/belluno')
+def belluno():
+    """
+    Render the Belluno page.
+
+    Returns:
+        str: Rendered HTML content for the Belluno page.
+    """
+    return render_template('belluno.html')
+
+
+@app.route('/treviso')
+def treviso():
+    """
+    Render the Treviso page.
+
+    Returns:
+        str: Rendered HTML content for the Treviso page.
+    """
+    return render_template('treviso.html')
+
+
+@app.route('/vicenza')
+def vicenza():
+    """
+    Render the Vicenza page.
+
+    Returns:
+        str: Rendered HTML content for the Vicenza page.
+    """
+    return render_template('vicenza.html')
+
+
+@app.route('/rovigo')
+def rovigo():
+    """
+    Render the Rovigo page.
+
+    Returns:
+        str: Rendered HTML content for the Rovigo page.
+    """
+    return render_template('rovigo.html')
+
+
+@app.route('/internal', methods=['GET', 'POST'])
+def internal():
+    """
+    Render the internal search page.
+
+    Returns:
+        str: Rendered HTML content for the internal search page.
+    """
+    form = QueryForm()
+    error_message = None
+
+    if form.validate_on_submit():
+        comune = form.person_name.data
+
+        """Obtain checkbox's values from form."""
+        piscina_filter = form.piscina_checkbox.data
+        accesso_disabili_filter = form.accesso_disabili_checkbox.data
+        fitness_filter = form.fitness_checkbox.data
+        sauna_filter = form.sauna_checkbox.data
+        aria_condizionata_filter = form.aria_condizionata_checkbox.data
+        animali_amessi_filter = form.animali_amessi_checkbox.data
+
+        """Update URL to include filters"""
+        fastapi_url = f'{FASTAPI_BACKEND_HOST}/query/{comune}?piscina={piscina_filter}&accesso_disabili={accesso_disabili_filter}&fitness={fitness_filter}&sauna={sauna_filter}&aria_condizionata={aria_condizionata_filter}&animali_ammessi={animali_amessi_filter}'
+
+        """Make a GET request to the FastAPI backend."""
+        response = requests.get(fastapi_url)
+
+        if response.status_code == 200:
+            """Extract and display the result from the FastAPI backend."""
+            data = response.json()
+            accomodations = data.get('risultati', [])
+
+            if accomodations:
+                result_strutture = []
+                for struttura in accomodations:
+                    result_item = {"nome": struttura["nome"]}
+                    if "link" in struttura:
+                        result_item["link"] = struttura["link"]
+                    if "indirizzo" in struttura:
+                        result_item["indirizzo"] = struttura["indirizzo"]
+                    if "telefono" in struttura:
+                        result_item["telefono"] = struttura["telefono"]
+                    result_strutture.append(result_item)
+
+                # Debug print to check the value of result_strutture
+                print("Result_strutture:", result_strutture)
+
+                if not result_strutture:
+                    result_strutture = [{"nome": f'No accommodations available for {comune}'}]
+            else:
+                result_strutture = [{"nome": f'No accommodations available for {comune}'}]
+
+            """Extract museum's information."""
+            musei_consigliati = data.get('musei_consigliati', [])
+            result_musei = ', '.join(musei_consigliati) if musei_consigliati else f'No recommended museums for {comune}'
+
+            return render_template('internal.html', form=form, result_strutture=result_strutture, result_musei=result_musei, error_message=error_message)
+        else:
+            error_message = f'Error: Unable to fetch accommodations for {comune} from FastAPI Backend'
+
+    return render_template('internal.html', form=form, result_strutture=None, result_musei=None, error_message=error_message)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
+
