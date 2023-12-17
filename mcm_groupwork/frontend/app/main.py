1c84b86cf89c9dafed1a2bcb81b649e5a276f6cc
MartaCarnio <891943@stud.unive.it>
2023-12-17 15:35:18 +0100

    Update main.py
    Some docstrings have been added.

--- mcm_groupwork/frontend/app/main.py
+++ mcm_groupwork/frontend/app/main.py
@@ -15,12 +15,14 @@ from wtforms.validators import DataRequired
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
-# Configuration for the FastAPI backend URL
+"""Configuration for the FastAPI backend URL."""
 FASTAPI_BACKEND_HOST = 'http://backend'
 BACKEND_URL = f'{FASTAPI_BACKEND_HOST}/query/'
 
-# Form class to handle query input from the user
 class QueryForm(FlaskForm):
+    """
+    Form class to handle query input from the user.
+    """
 
     person_name = StringField('Destination:')
     piscina_checkbox = BooleanField('Piscina Checkbox')
@@ -46,46 +48,96 @@ def index():
 
 @app.route('/padova')
 def padova():
+    """
+    Render the Padova page.
+
+    Returns:
+        str: Rendered HTML content for the Padova page.
+    """
     return render_template('padova.html')
 
 
 @app.route('/venezia')
 def venezia():
+    """
+    Render the Venezia page.
+
+    Returns:
+        str: Rendered HTML content for the Venezia page.
+    """
     return render_template('venezia.html')
 
 
 @app.route('/verona')
 def verona():
+    """
+    Render the Verona page.
+
+    Returns:
+        str: Rendered HTML content for the Verona page.
+    """
     return render_template('verona.html')
 
 
 @app.route('/belluno')
 def belluno():
+    """
+    Render the Belluno page.
+
+    Returns:
+        str: Rendered HTML content for the Belluno page.
+    """
     return render_template('belluno.html')
 
 
 @app.route('/treviso')
 def treviso():
+    """
+    Render the Treviso page.
+
+    Returns:
+        str: Rendered HTML content for the Treviso page.
+    """
     return render_template('treviso.html')
 
 
 @app.route('/vicenza')
 def vicenza():
+    """
+    Render the Vicenza page.
+
+    Returns:
+        str: Rendered HTML content for the Vicenza page.
+    """
     return render_template('vicenza.html')
 
 
 @app.route('/rovigo')
 def rovigo():
+    """
+    Render the Rovigo page.
+
+    Returns:
+        str: Rendered HTML content for the Rovigo page.
+    """
     return render_template('rovigo.html')
 
 
 @app.route('/internal', methods=['GET', 'POST'])
 def internal():
+    """
+    Render the internal search page.
+
+    Returns:
+        str: Rendered HTML content for the internal search page.
+    """
     form = QueryForm()
     error_message = None
 
     if form.validate_on_submit():
         comune = form.person_name.data
+
+        """Obtain checkbox's values from form."""
         piscina_filter = form.piscina_checkbox.data
         accesso_disabili_filter = form.accesso_disabili_checkbox.data
         fitness_filter = form.fitness_checkbox.data
@@ -93,14 +145,14 @@ def internal():
         aria_condizionata_filter = form.aria_condizionata_checkbox.data
         animali_amessi_filter = form.animali_amessi_checkbox.data
 
-        # Update URL to include filters
+        """Update URL to include filters"""
         fastapi_url = f'{FASTAPI_BACKEND_HOST}/query/{comune}?piscina={piscina_filter}&accesso_disabili={accesso_disabili_filter}&fitness={fitness_filter}&sauna={sauna_filter}&aria_condizionata={aria_condizionata_filter}&animali_ammessi={animali_amessi_filter}'
 
-        # Make a GET request to the FastAPI backend
+        """Make a GET request to the FastAPI backend."""
         response = requests.get(fastapi_url)
 
         if response.status_code == 200:
-            # Extract and display the result from the FastAPI backend
+            """Extract and display the result from the FastAPI backend."""
             data = response.json()
             accomodations = data.get('risultati', [])
 
@@ -120,7 +172,7 @@ def internal():
             else:
                 result_strutture = [{"nome": f'No accommodations available for {comune}'}]
 
-            # Extract museum's information
+            """Extract museum's information."""
             musei_consigliati = data.get('musei_consigliati', [])
             result_musei = ', '.join(musei_consigliati) if musei_consigliati else f'No recommended museums for {comune}'
 
